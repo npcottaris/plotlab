243690ee819fe0f7a138c4ddd2bf047f70951429
Nicolas Cottaris <cottaris@upenn.edu>
2020-03-23 21:40:51 -0400

    Ability to offset x- or y-axis only

--- @plotlab/offsetAxes.m
+++ @plotlab/offsetAxes.m
@@ -15,6 +15,7 @@ function offsetAxes(axesHandle, varargin)
 %
 % Optional key/value pairs:
 %    'offsetPercent'    - Percentage of the axis range for the offset
+%    'whichAxis'        - Which axis to offset: {'both', 'x-only','y-only'}
 %
 % For usage see:
 %   t_areaPlotUsingPrivateRecipe()
@@ -25,6 +26,7 @@ function offsetAxes(axesHandle, varargin)
 
     p = inputParser;
     p.addParameter('offsetPercent',0.03, @(x)(isnumeric(x)));
+    p.addParameter('whichAxis', 'both', @(x)(ismember(x, {'both', 'x-only', 'y-only'})));
     p.parse(varargin{:});
     
     XLim = get(axesHandle, 'XLim');
@@ -47,9 +49,12 @@ function offsetAxes(axesHandle, varargin)
     XLim(1) = newMin(XLim, XScale,p.Results.offsetPercent, signX);
     YLim(1) = newMin(YLim, YScale,p.Results.offsetPercent, signY);
     
-    set(axesHandle, 'XLim', XLim, 'YLim', YLim);
-    get(axesHandle, 'XMinorTick'); 
-    
+    if (ismember(p.Results.whichAxis, {'both', 'y-only'}))
+        set(axesHandle, 'XLim', XLim);
+    end
+    if (ismember(p.Results.whichAxis, {'both', 'x-only'}))
+        set(axesHandle, 'YLim', YLim);
+    end
 end
 
 function xMin = newMin(XLim, XScale,p, sign)
